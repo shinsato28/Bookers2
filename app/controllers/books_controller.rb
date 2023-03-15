1c67977a360a8093fcd3302c4e72d7ac24d7c695
EC2 Default User <ec2-user@ip-172-31-5-13.ap-northeast-1.compute.internal>
2023-03-15 03:07:17 +0000

    [Finish] kadai-3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def new
   end
 
@@ -50,5 +52,13 @@ class BooksController < ApplicationController
   private
     def book_params
       params.require(:book).permit(:title, :body)
+    end
+
+    def is_matching_login_user
+      book = Book.find(params[:id])
+      user = book.user
+      unless user.id == current_user.id
+        redirect_to books_path
+      end
     end
 end
